f8b02d36aa9b3eadbf051aed9957c085ea20091d
Yap Cheng Yee <chengyeeyap97@gmail.com>
2024-08-01 02:45:13 +0800

    Created extra 6 maps

---
+++ assets/themes/NPC.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.0" name="NPC" tilewidth="32" tileheight="32" tilecount="2296" columns="56">
+ <image source="Premade_Character_32x32_05.png" width="1792" height="1312"/>
+</tileset>
---
+++ assets/themes/music.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.0" name="music" tilewidth="32" tileheight="32" tilecount="768" columns="16">
+ <image source="6_Music_and_sport_32x32.png" width="512" height="1536"/>
+</tileset>
