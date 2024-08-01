f760d7016ebe7f78a2a2deb97bcbd9b0196904ad
Yap Cheng Yee <chengyeeyap97@gmail.com>
2024-08-01 11:54:25 +0800

    Working museum

--- Main.js
+++ Main.js
@@ -14,7 +14,7 @@ let config = {
         // debug: true
       }
     },
-    scene: [Lounge, Bathroom, Classroom, LoungeMedium, LoungeHard]
+    scene: [Lounge, Bathroom, Classroom, LoungeMedium, LoungeHard, ClassroomMedium, ClassroomHard]
   
 }
 
