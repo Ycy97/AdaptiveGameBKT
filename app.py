66d16e9937ca44098de13a58d91723bfa36bc7e4
Yap Cheng Yee <chengyeeyap97@gmail.com>
2024-08-15 00:00:54 +0800

    Added login and signup page

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, session, render_template, redirect, url_for
 from flask_cors import CORS
+from flask_bcrypt import Bcrypt
 import mysql.connector
 from pyBKT_model import BKT
 from bkt_model2 import update_knowledge
@@ -7,6 +8,8 @@ from bkt_model2 import update_knowledge
 app = Flask(__name__)
 # Allow CORS for all domains on all routes
 CORS(app, resources={r"/*": {"origins": "*"}})
+app.secret_key = 'cy_secretKey'
+bcrypt = Bcrypt(app)
 
 
 # MYSQL database connection configuration parameters
@@ -18,11 +21,19 @@ db_config = {
     'port': 3306
 }
 
+db_config2 = {
+    'host': "localhost",
+    'user': "root",
+    'password': "khcy6ycy",
+    'database': "recommendation_engine",
+    'port': 3306
+}
+
 print("Ready to connect to database")
 
 @app.route('/', methods=['GET'])
 def index():
-    return jsonify({"message": "Welcome to the Math Questions API!"})
+    return render_template('index.html')
 
 @app.route('/algebra', methods=['GET'])
 def get_algebra_questions():
@@ -106,6 +117,63 @@ def save_responses():
 
     return response
 
+#API to allow user to login into the game
+@app.route('/login', methods=['POST'])
+def gameLogin():
+    data = request.get_json()
+    username = data.get('username')
+    password = data.get('password')
+    conn = mysql.connector.connect(**db_config2)
+    cur = conn.cursor()
+    auth_query = "SELECT * FROM users WHERE studentID =%s"
+    cur.execute(auth_query, (username,))
+    user = cur.fetchone()
+
+    if user:
+        stored_password_hash = user[1]
+        if bcrypt.check_password_hash(stored_password_hash, password):
+            session['username'] = username
+            cur.close()
+            conn.close()
+            return jsonify({'message' : 'Login successful!'}),200
+            #Render the game
+        else:
+            cur.close()
+            conn.close()
+            return jsonify({'message': 'Invalid username or password'}), 401
+    else:
+        cur.close()
+        conn.close()
+        return jsonify({'message': 'Invalid username or password'}), 401
+
+#API to allow user to signup/register
+@app.route('/signup', methods=['POST'])
+def gameSignup():
+    data = request.get_json()
+    username = data.get('username')
+    password = data.get('password')
+    conn = mysql.connector.connect(**db_config2)
+    cur = conn.cursor()
+    auth_query = "SELECT * FROM users WHERE studentID =%s"
+    cur.execute(auth_query, (username,))
+    user = cur.fetchone()
+
+    if user:
+        return jsonify({'message': 'User already exists'}), 400
+    password_hash = bcrypt.generate_password_hash(password).decode('utf-8')
+
+    register_query = "INSERT INTO users (studentID, password_hash) VALUES (%s, %s)"
+    cur.execute(register_query, (username, password_hash))
+    conn.commit()
+    cur.close()
+    conn.close()
+    return jsonify({'message': 'User registered successfully'}), 201
+
+#API to logout user
+@app.route('/logout',methods=['POST'])
+def logout():
+    session.pop('username', None)
+    return jsonify({'message': 'Logged out successfully'}), 200
 
 if __name__ == '__main__':
     app.run(debug=True)
